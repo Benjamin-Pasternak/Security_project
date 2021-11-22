aef43e658e70bf654f1c265a089057e558681542
Lisk39 <paulhyu1234@gmail.com>
2021-11-21 20:32:29 -0500

    everything looks good

--- mongo.py
+++ mongo.py
@@ -4,17 +4,23 @@ import time
 import os
 import sys
 import certifi
+import json
+import rsa3
 
-# import rsa2
+def decrypt_connection_string():
+    file = open('./secrets.json')
+    data = json.load(file)
+    connect_string = rsa3.rsa_decrypt_message(data['string'], data['d'], data['n'])
+    connect_string = int(''.join([str(x) for x in connect_string]))
+    connect_string = connect_string.to_bytes((connect_string.bit_length() + 7) // 8, 'big').decode('utf-8')
+    return connect_string
+
 
 
 class mongodb_atlas_test:
     def __init__(self):
-        """
-        TODO: This client connection string needs to probabaly be secured. Need to figure out what
-        the best way to do this is.
-        """
-        self.client = pymongo.MongoClient('mongodb+srv://general_user:Snapdragon777apZ@cluster0.shbn7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+        # connection_uri =
+        self.client = pymongo.MongoClient(f'{decrypt_connection_string()}', tlsCAFile=certifi.where())
         self.db = self.client.myFirstDatabase
         self.collection = self.db.user_info
 
@@ -45,48 +51,3 @@ class mongodb_atlas_test:
         @param data: This is the data that is to be inserted into the database. Dictionary format.
         """
         self.collection.update_one(data, {'$set': data}, upsert=True)
-
-
-# if __name__ == "__main__":
-#     mongodb_atlas_test = mongodb_atlas_test()
-#     # publicKey1, e = rsa2.gen_public_key()
-#     # privateKey1 = rsa2.gen_private_key(publicKey1[0], e)
-#     # publicKey2, e = rsa2.gen_public_key()
-#     # privateKey2 = rsa2.gen_private_key(publicKey2[0], e)
-#     # data1 = {
-#     #     "username": "John",
-#     #     "password": "yomama",
-#     #     "publicKey": publicKey1
-#     # }
-#     data2 = {
-#         "username": "Bob",
-#         "password": "yo_mama",
-#         "publicKey": (13, 5)
-#     }
-#
-#     # data2 = {mongodb_atlas_test.collection.find( { "$text": { "$search": "Bob" } } )
-#
-#     the = mongodb_atlas_test.get_data('Bob')
-#     # the = mongodb_atlas_test.collection.find({ "$text": { "$search": "Bob" } } )
-#
-#     # the = mongodb_atlas_test.get_data({"$text": { "$search": "Bob" } })
-#     yoo = 'yo_mama'
-#     print(the)
-#     if not the:
-#         print('hmm')
-#     temp = str(the[0])
-#     print(temp.find('password'))
-#     print(temp[66])
-#     num = temp.find('password')
-#     num = num+12
-#     temp = temp[num:]
-#     print(temp.find("'"))
-#     num2 = temp.find("'")
-#     temp = temp[:num2]
-#     print(temp)
-#     if(temp == yoo):
-#         print('yay')
-#     #the2 = dict[(the)]
-#
-#    # usetemp = the2.get("username")
-#    # print(usetemp)
---
+++ rsa3.py
@@ -0,0 +1,183 @@
+from Crypto.Util.number import getPrime
+from functools import lru_cache
+import numpy as np
+import sys
+# from mongo import mongodb_atlas_test
+# from monggo import *
+import hashlib
+
+"""
+(n,e) = public_key
+(n,d) = private_key
+p, q = large prime numbers 
+phi = (p - 1) * (q - 1) = totient function 
+d = mod_inv_euclid_extended(e, phi)
+-----------
+we keep d and n locally, and hidden in a file or something 
+we push n and d to the mongo server 
+"""
+
+
+def gen_p_q():
+    while True:
+        p = getPrime(512)
+        q = getPrime(512)
+        if p != q:
+            return p, q
+
+
+def gen_n(p, q):
+    return p * q
+
+
+def gen_phi(p, q):
+    return (p - 1) * (q - 1)
+
+
+def gen_e(phi):
+    e = 3
+    while e < phi:
+        if gcd_euclid(e, phi) == 1:
+            return e
+        e += 1
+
+
+@lru_cache(maxsize=None)
+def gcd_euclid(a, b):
+    if b == 0:
+        return a
+    return gcd_euclid(b, a % b)
+
+
+@lru_cache(maxsize=None)
+def gcd_euclid_extended(a, b):
+    if b == 0:
+        return 1, 0, a
+    x, y, d = gcd_euclid_extended(b, a % b)
+    return y, x - (a // b) * y, d
+
+
+@lru_cache(maxsize=None)
+def mod_inv_euclid_extended(a, m):
+    x, y, d = gcd_euclid_extended(a, m)
+    if d == 1:
+        return x % m
+    return None
+
+
+def gen_d(e, phi):
+    return mod_inv_euclid_extended(e, phi)
+
+
+def encrypt_block(m, e, n):
+    return pow(m, e, n)
+
+
+def decrypt_block(c, d, n):
+    return pow(c, d, n)
+
+
+def rsa_encrypt_message(m, e, n):
+    blocks = [int(x) for x in str(m)]
+    c = []
+    for block in blocks:
+        c.append(encrypt_block(block, e, n))
+    return c
+
+
+def rsa_decrypt_message(c, d, n):
+    blocks = [int(x) for x in c]
+    m = []
+    for block in blocks:
+        m.append(decrypt_block(block, d, n))
+    return m
+
+
+"""
+--------------------------------------------------------------------------------
+---------------------------- This is for the signup ----------------------------
+--------------------------------------------------------------------------------
+"""
+
+
+def gen_keys():
+    p, q = gen_p_q()
+    n = gen_n(p, q)
+    phi = gen_phi(p, q)
+    e = gen_e(phi)
+    # n = str(n)
+    # e = str(e)
+    d = gen_d(e, phi)
+
+    return (n, e), (n, d)
+
+
+# def gen_private_key(n, e):
+#
+#     return d
+
+# database = mongodb_atlas_test()
+#
+# def hash_password(password):
+#     return hashlib.sha256(password.encode()).hexdigest()
+#
+# def signup(username, password):
+#     public_key, e = gen_public_key()
+#     private_key = gen_private_key(public_key[0], e)
+#     print(public_key)
+#     print(private_key)
+#     with open('keys.txt', 'w') as f:
+#         f.write(str(public_key) + '\n')
+#         f.write(str(private_key))
+#     # hash the password since we don't want server to know the password
+#     # is this safer to do on our end or server end?
+#     database.insert_user({'username': username, 'password' : hash_password(password), 'public key' :public_key })
+#
+#
+#
+
+def main():
+    # p, q = gen_p_q()
+    # n = gen_n(p, q)
+    # phi = gen_phi(p, q)
+    # e = gen_e(phi)
+    # d = gen_d(e, phi)
+    public_key, private_key = gen_keys()
+    print("e", public_key[1])
+    print("pub key:", public_key)
+    print("n?", public_key[0])
+    # private_key = gen_private_key(public_key[0], e)
+    test = str(private_key)
+    test = test.replace("(", '')
+    print(test)
+    n = int(test.partition(', ')[0])
+    # n = public_key[0]
+    print(type(n))
+    e = public_key[1]
+    d = private_key[1]
+    print("d", d)
+    # d = private_key[1]
+    # print('THE JOKE IS ON YOU')
+    # n = int(gen_public_key().partition(' ')[0])
+    # e = int(gen_public_key().partition(' ')[2])
+    # d = int(gen_private_key(n, e))
+    m = int.from_bytes(
+        b'mongodb+srv://general_user:Snapdragon777apZ@cluster0.shbn7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        'big')
+    # print(m)
+    c = rsa_encrypt_message(m, e, n)
+    print('C: ', c)
+    print('n: ', n)
+    print('d: ', d)
+    m2 = rsa_decrypt_message(c, d, n)
+    print(m2)
+    m2 = int(''.join([str(x) for x in m2]))
+    # m2 = int(m2)
+    m2 = m2.to_bytes((m2.bit_length() + 7) // 8, 'big').decode('utf-8')
+    print(m2)
+    # b = m2.to_bytes(, 'big')
+    # b = b.decode('utf-8')
+    # print(b)
+
+# if __name__ == '__main__':
+#     main()
